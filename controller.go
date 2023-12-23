e54bac517bf3b23b560e79c897e88afdac4c0170
Kamigen <46357922+Edouard127@users.noreply.github.com>
2023-12-23 11:12:50 -0500

    Initial commit

---
+++ controller.go
@@ -0,0 +1,161 @@
+package tor_controller
+
+import (
+	"fmt"
+	"net/textproto"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Signal int
+
+const (
+	Reload = iota
+	Shutdown
+	Dump
+	Debug
+	Halt
+	NewCircuit
+	ClearCircuit
+	Heartbeat
+	Dormant
+	Active
+)
+
+func (s Signal) String() string {
+	return [...]string{
+		"RELOAD",
+		"SHUTDOWN",
+		"DUMP",
+		"DEBUG",
+		"HALT",
+		"NEWNYM",
+		"CLEARDNSCACHE",
+		"HEARTBEAT",
+		"DORMANT",
+		"ACTIVE",
+	}[s]
+}
+
+type Controller struct {
+	*textproto.Conn
+
+	mu sync.Mutex
+
+	lastSignal     Signal
+	lastSignalTime time.Time
+}
+
+// NewController creates a new controller connection to the given address.
+// The address should be in the form "host:port". If the address is empty,
+// the default address will be 127.0.0.1:9051.
+func NewController(addr string) (*Controller, error) {
+	if addr == "" {
+		addr = "127.0.0.1:9051"
+	}
+	conn, err := textproto.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return &Controller{Conn: conn, lastSignal: -1}, nil
+}
+
+// Signal sends a signal to the Tor process.
+// If the same signal is sent twice within 10 seconds, the second signal is
+// ignored.
+// See https://gitweb.torproject.org/torspec.git/tree/control-spec.txt#n102
+func (c *Controller) Signal(signal Signal) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if signal == c.lastSignal && time.Since(c.lastSignalTime) < 10*time.Second+500*time.Millisecond {
+		return nil // because this is a no-op
+	}
+	_, _, err := c.makeRequest("SIGNAL " + signal.String())
+	if err != nil {
+		return err
+	}
+	c.lastSignal = signal
+	c.lastSignalTime = time.Now()
+	return nil
+}
+
+func (c *Controller) GetAddress() (string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.getInfo("address")
+}
+
+func (c *Controller) GetBytesRead() (int, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.getInfoInt("traffic/read")
+}
+
+func (c *Controller) GetBytesWritten() (int, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.getInfoInt("traffic/written")
+}
+
+func (c *Controller) GetVersion() (string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.getInfo("version")
+}
+
+// AuthenticateNone authenticate to controller without password
+func (c *Controller) AuthenticateNone() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	_, _, err := c.makeRequest("AUTHENTICATE")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Authenticate to controller with password.
+func (c *Controller) Authenticate(password string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	_, _, err := c.makeRequest("AUTHENTICATE " + password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Controller) makeRequest(request string) (int, string, error) {
+	id, err := c.Cmd(request)
+	if err != nil {
+		return 0, "", err
+	}
+	c.StartResponse(id)
+	defer c.EndResponse(id)
+	return c.ReadResponse(250)
+}
+
+func (c *Controller) getInfo(key string) (string, error) {
+	_, msg, err := c.makeRequest("GETINFO " + key)
+	if err != nil {
+		return "", err
+	}
+	lines := strings.Split(msg, "\n")
+	for _, line := range lines {
+		parts := strings.SplitN(line, "=", 2)
+		if parts[0] == key {
+			return parts[1], nil
+		}
+	}
+	return "", fmt.Errorf(key + " not found")
+}
+
+func (c *Controller) getInfoInt(key string) (int, error) {
+	s, err := c.getInfo(key)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Edouard127/tor_controller
+
+go 1.21.0
